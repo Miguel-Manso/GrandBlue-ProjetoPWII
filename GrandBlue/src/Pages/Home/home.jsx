2eec25d983af0419d7435773eb092fd8d5a8a5f9
Lulão Spada <lucasspadoniferreira@gmail.com>
2022-09-29 22:45:32 -0300

    Home

--- GrandBlue/src/Pages/Home/home.jsx
+++ GrandBlue/src/Pages/Home/home.jsx
@@ -1,5 +1,5 @@
 import { Navbar } from "../../Components/Navbar/navbar.jsx"
-import { Container } from "./style.jsx"
+import { Container, Paragrafo, Paragrafos, Secao, Titulo, Titulos, Texto} from "./style.jsx"
 
 
 export function Home () {
@@ -7,6 +7,28 @@ export function Home () {
         
         <Container>
             <Navbar />
+            <Secao>
+                <Titulos>
+                    <Titulo>Bem-vindo à Grand Blue</Titulo>
+                    <Paragrafo>Desfrute de experiências incríveis conosco</Paragrafo>
+                </Titulos>
+      
+            </Secao>
+                    <Paragrafos>
+                        <Texto>
+                        O mergulho é definido como o ato de submergir dentro d'água que pode ser praticado 
+                        de forma independente e livre, apenas controlando a respiração, como também de modo 
+                        dependente, semi-dependente e autônomo, com a ajuda de um aparelho de respiração.
+                        </Texto>
+                        <Texto>
+                        O mergulho, modalidade desportiva aquática praticada desde tempos antigos, 
+                        está dividido em dois tipos: um que é feito em piscinas, a partir de pranchas 
+                        e trampolins e cujo objetivo é realizar acrobacias, pontuadas por juízes, 
+                        antes da entrada na água
+                        </Texto>
+                    </Paragrafos>
+                
+                
         </Container>
     )
 }
--- GrandBlue/src/Pages/Home/style.jsx
+++ GrandBlue/src/Pages/Home/style.jsx
@@ -1,9 +1,78 @@
 import styled from "styled-components"
+import banner from "../../Assets/Banner.png"
 
 const Container = styled.div`
-    background-color: aqua;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #414141;
     height: 100vh;
    
-`
+`;
+const Secao = styled.div`
 
-export {Container}
+    display: flex;
+    justify-content:center;
+    align-items:center;
+    align-content:center;
+    height:70%;
+    width:100%;
+    background-image: url(${banner});
+    background-size: cover;
+    
+    box-shadow: 0px 4px 4px 2px rgba(0,0,0,0.1);
+
+`;
+
+const Titulos = styled.div`
+
+    display:flex;
+    flex-direction:column;
+    height:20%;
+    justify-items: center;
+    align-items: center;
+
+`;
+const Paragrafos = styled.div`
+
+    display:flex;   
+    gap: 25px;
+    justify-items: center;
+    align-items: center;
+    width: 100%;
+    height: 20%;
+
+`;
+const Titulo = styled.h5`
+
+    font-size: 5rem;
+    @import url('https://fonts.googleapis.com/css2?family=Abyssinica+SIL&display=swap');
+    font-family: 'Abyssinica', Georgia, 'Times New Roman', Times, serif;
+    color: white;
+    font-weight: lighter;
+  
+`;
+const Paragrafo = styled.p`
+
+    font-size: 1.5rem;
+    @import url('https://fonts.googleapis.com/css2?family=Alata&display=swap');
+    font-family: 'Alata', Verdana, Geneva, Tahoma, sans-serif;
+    color: white;
+    justify-content: center;
+    text-transform: uppercase;
+  
+`;
+const Texto = styled.p`
+
+    font-size: 1rem;
+    @import url('https://fonts.googleapis.com/css2?family=Abyssinica+SIL&display=swap');
+    font-family: 'Abyssinica', Georgia, 'Times New Roman', Times, serif;
+    color: white;
+    font-weight: lighter;
+    text-align: justify;
+    width: 50%;
+    margin: 5rem;
+   
+`;
+
+export { Container, Secao, Titulo, Paragrafo, Titulos, Paragrafos, Texto } 
